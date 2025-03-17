4044f1ab146be83499b6d6404c5193b408dd7e15
Brandon Benoit <brandon@digitalkatana.dev>
2025-03-17 12:40:29 -0700

    uncommented test in login route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -25,13 +25,13 @@ router.post('/login', async (req, res) => {
 	const { email, password } = req?.body;
 	let user;
 
-	// const base = 'vervecloud.com';
-	// const test = email?.split('@')[1];
+	const base = 'vervecloud.com';
+	const test = email?.split('@')[1];
 
-	// if (test !== base) {
-	// 	errors.login = 'Error, must have a valid Verve email.';
-	// 	return res.status(400).json(errors);
-	// }
+	if (test !== base) {
+		errors.login = 'Error, must have a valid Verve email.';
+		return res.status(400).json(errors);
+	}
 
 	try {
 		user = await User.findOne({ email }).populate('profile');
